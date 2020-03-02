c122836474874810f883e077d674af882166129d
DTechio <danieltechio@hotmail.com>
2020-03-02 11:48:40 -0300

    Up

---
+++ Ordenadores/Ordenadores/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Ordenadores
+{
+    partial class Ordenadores
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ordenadores));
+            this.Gerar = new System.Windows.Forms.Button();
+            this.Ordenar = new System.Windows.Forms.Button();
+            this.Limpar = new System.Windows.Forms.Button();
+            this.Insertion = new System.Windows.Forms.RadioButton();
+            this.Selection = new System.Windows.Forms.RadioButton();
+            this.Heap = new System.Windows.Forms.RadioButton();
+            this.Bubble = new System.Windows.Forms.RadioButton();
+            this.Quick = new System.Windows.Forms.RadioButton();
+            this.txtQntd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.labelTempo = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Gerar
+            // 
+            this.Gerar.Location = new System.Drawing.Point(650, 258);
+            this.Gerar.Name = "Gerar";
+            this.Gerar.Size = new System.Drawing.Size(75, 23);
+            this.Gerar.TabIndex = 1;
+            this.Gerar.Text = "Gerar";
+            this.Gerar.UseVisualStyleBackColor = true;
+            this.Gerar.Click += new System.EventHandler(this.Gerar_Click);
+            // 
+            // Ordenar
+            // 
+            this.Ordenar.Location = new System.Drawing.Point(650, 304);
+            this.Ordenar.Name = "Ordenar";
+            this.Ordenar.Size = new System.Drawing.Size(75, 23);
+            this.Ordenar.TabIndex = 2;
+            this.Ordenar.Text = "Ordenar";
+            this.Ordenar.UseVisualStyleBackColor = true;
+            this.Ordenar.Click += new System.EventHandler(this.Ordenar_Click);
+            // 
+            // Limpar
+            // 
+            this.Limpar.Location = new System.Drawing.Point(650, 350);
+            this.Limpar.Name = "Limpar";
+            this.Limpar.Size = new System.Drawing.Size(75, 23);
+            this.Limpar.TabIndex = 3;
+            this.Limpar.Text = "Limpar";
+            this.Limpar.UseVisualStyleBackColor = true;
+            this.Limpar.Click += new System.EventHandler(this.Limpar_Click);
+            // 
+            // Insertion
+            // 
+            this.Insertion.AutoSize = true;
+            this.Insertion.Location = new System.Drawing.Point(80, 350);
+            this.Insertion.Name = "Insertion";
+            this.Insertion.Size = new System.Drawing.Size(87, 17);
+            this.Insertion.TabIndex = 4;
+            this.Insertion.TabStop = true;
+            this.Insertion.Text = "Insertion Sort";
+            this.Insertion.UseVisualStyleBackColor = true;
+            // 
+            // Selection
+            // 
+            this.Selection.AutoSize = true;
+            this.Selection.Location = new System.Drawing.Point(80, 327);
+            this.Selection.Name = "Selection";
+            this.Selection.Size = new System.Drawing.Size(91, 17);
+            this.Selection.TabIndex = 5;
+            this.Selection.TabStop = true;
+            this.Selection.Text = "Selection Sort";
+            this.Selection.UseVisualStyleBackColor = true;
+            // 
+            // Heap
+            // 
+            this.Heap.AutoSize = true;
+            this.Heap.Location = new System.Drawing.Point(80, 304);
+            this.Heap.Name = "Heap";
+            this.Heap.Size = new System.Drawing.Size(73, 17);
+            this.Heap.TabIndex = 6;
+            this.Heap.TabStop = true;
+            this.Heap.Text = "Heap Sort";
+            this.Heap.UseVisualStyleBackColor = true;
+            // 
+            // Bubble
+            // 
+            this.Bubble.AutoSize = true;
+            this.Bubble.Location = new System.Drawing.Point(80, 281);
+            this.Bubble.Name = "Bubble";
+            this.Bubble.Size = new System.Drawing.Size(80, 17);
+            this.Bubble.TabIndex = 7;
+            this.Bubble.TabStop = true;
+            this.Bubble.Text = "Bubble Sort";
+            this.Bubble.UseVisualStyleBackColor = true;
+            // 
+            // Quick
+            // 
+            this.Quick.AutoSize = true;
+            this.Quick.Location = new System.Drawing.Point(80, 258);
+            this.Quick.Name = "Quick";
+            this.Quick.Size = new System.Drawing.Size(75, 17);
+            this.Quick.TabIndex = 8;
+            this.Quick.TabStop = true;
+            this.Quick.Text = "Quick Sort";
+            this.Quick.UseVisualStyleBackColor = true;
+            // 
+            // txtQntd
+            // 
+            this.txtQntd.Location = new System.Drawing.Point(344, 191);
+            this.txtQntd.Name = "txtQntd";
+            this.txtQntd.Size = new System.Drawing.Size(100, 20);
+            this.txtQntd.TabIndex = 9;
+            this.txtQntd.TextChanged += new System.EventHandler(this.txtQntd_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(303, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Gerar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "números.";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(80, 12);
+            this.txtNum.Multiline = true;
+            this.txtNum.Name = "txtNum";
+            this.txtNum.ReadOnly = true;
+            this.txtNum.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNum.Size = new System.Drawing.Size(645, 173);
+            this.txtNum.TabIndex = 12;
+            // 
+            // labelTempo
+            // 
+            this.labelTempo.AutoSize = true;
+            this.labelTempo.Location = new System.Drawing.Point(341, 227);
+            this.labelTempo.Name = "labelTempo";
+            this.labelTempo.Size = new System.Drawing.Size(0, 13);
+            this.labelTempo.TabIndex = 13;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(386, 227);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(0, 13);
+            this.labelTime.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(300, 281);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Ordenadores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelTempo);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQntd);
+            this.Controls.Add(this.Quick);
+            this.Controls.Add(this.Bubble);
+            this.Controls.Add(this.Heap);
+            this.Controls.Add(this.Selection);
+            this.Controls.Add(this.Insertion);
+            this.Controls.Add(this.Limpar);
+            this.Controls.Add(this.Ordenar);
+            this.Controls.Add(this.Gerar);
+            this.Name = "Ordenadores";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ordenadores";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Gerar;
+        private System.Windows.Forms.Button Ordenar;
+        private System.Windows.Forms.Button Limpar;
+        private System.Windows.Forms.RadioButton Insertion;
+        private System.Windows.Forms.RadioButton Selection;
+        private System.Windows.Forms.RadioButton Heap;
+        private System.Windows.Forms.RadioButton Bubble;
+        private System.Windows.Forms.RadioButton Quick;
+        private System.Windows.Forms.TextBox txtQntd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label labelTempo;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Ordenadores/Ordenadores/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ordenadores
+{
+    public partial class Ordenadores : Form
+    {
+        List<int> generatedNums = new List<int>();
+        List<int> generatedSorted = new List<int>();
+
+        public Ordenadores()
+        {
+            InitializeComponent();
+        }
+
+        private void txtQntd_TextChanged_1(object sender, EventArgs e)
+        {
+            // Max de caracteres 4 no txtQntd
+            txtQntd.MaxLength = 4;
+        }
+
+        private void LimparTudo()
+        {
+            // Limpa TUDO
+            txtQntd.Clear();
+            txtNum.Clear();
+            generatedNums.Clear();
+            generatedSorted.Clear();
+            labelTempo.Text = "";
+            labelTime.Text = "";
+           
+            var checkedRadio = this.Controls.OfType<RadioButton>()
+                                     .FirstOrDefault(r => r.Checked);
+            if (checkedRadio != null)
+            {
+                checkedRadio.Checked = false;
+            }
+            
+        }
+
+        private void Gerar_Click(object sender, EventArgs e)
+        {
+            //Limpa tudo
+            txtNum.Clear();
+            generatedSorted.Clear();
+            generatedNums.Clear();
+            labelTempo.Text = "";
+            labelTime.Text = "";
+            if (String.IsNullOrEmpty(txtQntd.Text) || txtQntd.Text == "0")
+            {
+                // MSG se caixa de txt tiver vazio/zerada
+                MessageBox.Show("Insira uma quantidade de números válida!");
+            }
+            else
+            {
+                // Gera qntd de num aleatorios digitada no txt
+                int valor = Convert.ToInt32(txtQntd.Text);
+                Random randNum = new Random();
+
+                for (int i = 0; i < valor; i++)
+                {
+                    int num = randNum.Next(100);
+                    if (i != valor - 1)
+                    {
+                        txtNum.AppendText(num.ToString() + ", ");
+                        generatedNums.Add(num);
+                    }
+                    else
+                    {
+                        txtNum.AppendText(num.ToString() + ".");
+                        generatedNums.Add(num);
+                        generatedSorted.Add(num);
+                    }
+
+                }
+                
+            }
+            
+        }
+
+        private void Limpar_Click(object sender, EventArgs e)
+        {
+            // Botao exec metod LimparTudo
+            LimparTudo();
+        }
+
+        private void Ordenar_Click(object sender, EventArgs e)
+        {
+            // Ordena no algoritmo selecionado no radioButton
+            var checkedRadio = this.Controls.OfType<RadioButton>()
+                                      .FirstOrDefault(r => r.Checked);
+            List<Int32> generatedSorted = new List<Int32>(generatedNums);
+            if (generatedNums.Count == 0)
+            {
+                MessageBox.Show("Favor gerar os números a serem ordenados!");
+            }
+            else if (checkedRadio == null)
+            {
+                MessageBox.Show("Escolha uma opção de ordenação!");
+            }
+            else if (checkedRadio.Text == "Quick Sort")
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                int inicio = 0;
+                int fim = generatedSorted.Count - 1;
+                quickSort(generatedSorted, inicio, fim);
+                watch.Stop();
+                var elapsedMs = watch.Elapsed.TotalMilliseconds;
+                labelTempo.Text = "Tempo";
+                labelTime.Text = elapsedMs.ToString() + " ms";
+            }
+            else if (checkedRadio.Text == "Bubble Sort")
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                int tamanho = generatedSorted.Count;
+                int comparacoes = 0;
+                int trocas = 0;
+
+                for (int i = tamanho - 1; i >= 1; i--)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        comparacoes++;
+                        if (generatedSorted[j] > generatedSorted[j + 1])
+                        {
+                            int aux = generatedSorted[j];
+                            generatedSorted[j] = generatedSorted[j + 1];
+                            generatedSorted[j + 1] = aux;
+                            trocas++;
+                        }
+                    }
+                }
+                watch.Stop();
+                var elapsedMs = watch.Elapsed.TotalMilliseconds;
+                labelTempo.Text = "Tempo";
+                labelTime.Text = elapsedMs.ToString() + " ms";
+            }
+            else if (checkedRadio.Text == "Heap Sort")
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                buildMaxHeap(generatedSorted);
+                int n = generatedSorted.Count;
+
+                for (int i = generatedSorted.Count - 1; i > 0; i--)
+                {
+                    swap(generatedSorted, i, 0);
+                    maxHeapify(generatedSorted, 0, --n);
+                }
+                watch.Stop();
+                var elapsedMs = watch.Elapsed.TotalMilliseconds;
+                labelTempo.Text = "Tempo";
+                labelTime.Text = elapsedMs.ToString() + " ms";
+            }
+            else if (checkedRadio.Text == "Selection Sort")
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                int min, aux;
+
+                for (int i = 0; i < generatedSorted.Count - 1; i++)
+                {
+                    min = i;
+
+                    for (int j = i + 1; j < generatedSorted.Count; j++)
+                        if (generatedSorted[j] < generatedSorted[min])
+                            min = j;
+
+                    if (min != i)
+                    {
+                        aux = generatedSorted[min];
+                        generatedSorted[min] = generatedSorted[i];
+                        generatedSorted[i] = aux;
+                    }
+                }
+                watch.Stop();
+                var elapsedMs = watch.Elapsed.TotalMilliseconds;
+                labelTempo.Text = "Tempo";
+                labelTime.Text = elapsedMs.ToString() + " ms";
+            }
+            else if (checkedRadio.Text == "Insertion Sort")
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                int i, j, atual;
+                for (i = 1; i < generatedSorted.Count; i++)
+                {
+                    atual = generatedSorted[i];
+                    j = i;
+                    while ((j > 0) && (generatedSorted[j - 1] > atual))
+                    {
+                        generatedSorted[j] = generatedSorted[j - 1];
+                        j = j - 1;
+                    }
+                    generatedSorted[j] = atual;
+                }
+                watch.Stop();
+                var elapsedMs = watch.Elapsed.TotalMilliseconds;
+                labelTempo.Text = "Tempo";
+                labelTime.Text = elapsedMs.ToString() + " ms";
+            }
+            txtNum.Clear();
+            for (int i = 0; i < generatedSorted.Count; i++)
+            {
+                if (i != generatedSorted.Count - 1)
+                {
+                    txtNum.AppendText(generatedSorted[i] + ", ");
+                }
+                else
+                {
+                    txtNum.AppendText(generatedSorted[i] + ".");
+                }
+
+            }
+        }
+
+        private static void quickSort(List<int> generatedSorted, int inicio, int fim)
+        {
+            if (inicio < fim)
+            {
+                int p = generatedSorted[inicio];
+                int i = inicio + 1;
+                int f = fim;
+
+                while (i <= f)
+                {
+                    if (generatedSorted[i] <= p)
+                    {
+                        i++;
+                    }
+                    else if (p < generatedSorted[f])
+                    {
+                        f--;
+                    }
+                    else
+                    {
+                        int troca = generatedSorted[i];
+                        generatedSorted[i] = generatedSorted[f];
+                        generatedSorted[f] = troca;
+                        i++;
+                        f--;
+                    }
+                }
+
+                generatedSorted[inicio] = generatedSorted[f];
+                generatedSorted[f] = p;
+
+                quickSort(generatedSorted, inicio, f - 1);
+                quickSort(generatedSorted, f + 1, fim);
+            }
+        }
+
+        private static void buildMaxHeap(List<int> generatedSorted)
+        {
+            for (int i = generatedSorted.Count / 2 - 1; i >= 0; i--)
+            {
+                maxHeapify(generatedSorted, i, generatedSorted.Count);
+            }
+        }
+
+        private static void maxHeapify(List<int> generatedSorted, int pos, int n)
+        {
+            int max = 2 * pos + 1, right = max + 1;
+            if (max < n)
+            {
+                if (right < n && generatedSorted[max] < generatedSorted[right])
+		        {
+                    max = right;
+                }
+                if (generatedSorted[max] > generatedSorted[pos])
+                {
+                    swap(generatedSorted, max, pos);
+                    maxHeapify(generatedSorted, max, n);
+                }
+            }
+        }
+
+        private static void swap(List<int> generatedSorted, int j, int aposJ)
+        {
+            int aux = generatedSorted[j];
+            generatedSorted[j] = generatedSorted[aposJ];
+            generatedSorted[aposJ] = aux;
+        }
+
+
+    }
+}
